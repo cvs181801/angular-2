fd247ba6dea4e618683c293cbd5a1955a50d7a73
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-09-29 14:08:39 -0700

    save progress on angular tutorial

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,14 +5,20 @@ import { Component, OnInit } from '@angular/core';
   //selector: '[app-servers]', //this is a way to essentially make app-servers an attribute, and select by attribute (like html attribute)
   selector: '.app-servers', //this is a way to select by class name
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  styles: [`.moreThan5 { color: white;}`]
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreated = false;
+  displayButtonText = 'Display Details';
+  displaySecretText = false;
   serverName = 'Test server';
+  secretText = '';
   serverCreationStatus = 'no server was created!';
   servers = ['spongebobServer', 'patrickStarServer']
+  timestamps = [];
+  
   constructor() { 
     setTimeout(()=>{
       this.allowNewServer = true
@@ -29,4 +35,31 @@ export class ServersComponent implements OnInit {
     //console.log(event)
     this.serverName = (<HTMLInputElement>event.target).value;
   }
+  onDisplaySecretText() {
+    if(this.displaySecretText === false){
+      this.displaySecretText = true;
+      this.secretText = `top secret engineering text displayed!`;
+      this.displayButtonText = 'Hide Details';
+      this.timestamps.push(new Date());
+    }
+    else {
+      this.displaySecretText = false;
+      this.secretText = ``;
+      this.displayButtonText = 'Display Details';
+      this.timestamps.push(new Date());
+    }
+  }
+  setColor() {
+    return this.timestamps.length >= 5 ? 'blue' : 'none'
+  }
+  // constructor() {
+  //   this.timestamps.length >= 5  ? 'moreThan5' : ''
+  // }
 }
+
+
+//add a button which says 'display details'
+//add a paragraph w text content of your choice
+//when button is clicked, display text. when clicked again, hide text.
+//log all button clicks in an array and output that array below the secret paragraph (such as timestamps of when clicked)
+//starting at the 5th timestamp, give all future log items a blue background (ngStyle) and white color (ngClass)
